38f80f17b12d1374cd520227048605f7b76c963e
Naz <bluspringindohq@gmail.com>
2025-03-23 00:08:22 +0800

    Improve packet encoding by using variable buffer variants

--- fabric/src/main/java/com/github/cao/awa/annuus/AnnuusicClient.java
+++ fabric/src/main/java/com/github/cao/awa/annuus/AnnuusicClient.java
@@ -9,13 +9,14 @@ import com.github.cao.awa.annuus.network.packet.client.play.chunk.data.Collected
 import com.github.cao.awa.annuus.network.packet.server.notice.NoticeServerAnnuusPayload;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientConfigurationConnectionEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientConfigurationNetworking;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 
 public class AnnuusicClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientConfigurationConnectionEvents.START.register((handler, client) -> {
-            handler.sendPacket(NoticeServerAnnuusPayload.createPacket());
+            ClientConfigurationNetworking.send(NoticeServerAnnuusPayload.createData());
         });
 
         ClientPlayNetworking.registerGlobalReceiver(CollectedBlockUpdatePayload.IDENTIFIER, (packet, context) -> {
