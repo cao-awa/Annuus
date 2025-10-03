69258d47dd9a46202d10df8401437862904cb42b
cao-awa <cao.awa.www@gmail.com>
2025-10-04 04:42:07 +0800

    Fix bug of unable to launch without '1.21.9' Minecraft. (#23 ）

--- common/src/main/java/com/github/cao/awa/annuus/mixin/server/MinecraftServerMixin.java
+++ common/src/main/java/com/github/cao/awa/annuus/mixin/server/MinecraftServerMixin.java
@@ -1,21 +1,13 @@
 package com.github.cao.awa.annuus.mixin.server;
 
 import com.github.cao.awa.annuus.server.AnnuusServer;
-import com.mojang.datafixers.DataFixer;
-import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
-import net.minecraft.server.SaveLoader;
-import net.minecraft.util.ApiServices;
-import net.minecraft.world.chunk.ChunkLoadProgress;
-import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.net.Proxy;
 
 @Mixin(MinecraftServer.class)
 abstract public class MinecraftServerMixin {
@@ -23,10 +15,10 @@ abstract public class MinecraftServerMixin {
     private PlayerManager playerManager;
 
     @Inject(
-            method = "<init>",
-            at = @At("RETURN")
+            method = "loadWorld",
+            at = @At("HEAD")
     )
-    public void setupServer(Thread serverThread, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, Proxy proxy, DataFixer dataFixer, ApiServices apiServices, ChunkLoadProgress chunkLoadProgress, CallbackInfo ci) {
+    public void setupServer(CallbackInfo ci) {
         AnnuusServer.setupServerPlayerManager(this.playerManager);
     }
 }
